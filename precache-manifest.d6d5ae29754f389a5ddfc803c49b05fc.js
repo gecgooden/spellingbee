cd2d507ac16e91c0262f40a6f5f68cf2d89ea4ba
gecgooden <gecgooden@gmail.com>
2020-06-21 11:16:50 +0000

    Deploying to gh-pages from  @ f872ed0656c5a4da066d6efc219e6dd8af949f6b 🚀

---
+++ precache-manifest.d6d5ae29754f389a5ddfc803c49b05fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a492d1ff55166d370d8a6ea23d063cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87de4d5061e9ec66da5",
+    "url": "/static/css/main.9a432363.chunk.css"
+  },
+  {
+    "revision": "e46a2f284b1e93e297b2",
+    "url": "/static/js/2.891faab5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.891faab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87de4d5061e9ec66da5",
+    "url": "/static/js/main.d8951cb4.chunk.js"
+  },
+  {
+    "revision": "9ba16492e23156032bc2",
+    "url": "/static/js/runtime-main.ebdca942.js"
+  }
+]);
