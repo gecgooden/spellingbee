0ff31a5802179c5e7e1b6598c7765fd18f842686
George Gooden <george@gecgooden.com>
2020-06-21 19:08:23 +0800

    it's working!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import pangrams from './words/pangrams.json';
+import { HexGrid, Layout, Hexagon, Text, Pattern } from 'react-hexgrid';
+import shuffle from './shuffle';
+import words from './words/words.json';
+
+const pangram = pangrams[Math.floor(Math.random() * pangrams.length)];
+console.log(pangram);
+let letters = shuffle(
+  Array.from(
+    new Set<string>(pangram.split('')).values()
+  ).map(letter => letter.toUpperCase())
+);
+const requiredLetter = letters.pop();
+letters = letters.splice(0);
 
 function App() {
+  const [selectedLetters, setLetters] = useState<string[]>([]);
+  const [correctWords, setCorrectWords] = useState<string[]>([]);
+
+  const checkWord = (word: string) => {
+    console.log(word);
+    if (word.indexOf(requiredLetter) >= 0 && words.indexOf(word.toLowerCase()) >= 0) {
+      setCorrectWords([...correctWords, word]);
+    }
+    setLetters([]);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
+      <h1>{selectedLetters.join('')}</h1>
+      <HexGrid width={800} height={800} viewBox="-50 -50 100 100">
+        <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
+          <Hexagon q={0} r={0} s={0} onClick={() => setLetters([...selectedLetters, requiredLetter])}>
+            <Text>{requiredLetter}</Text>
+          </Hexagon>
+
+          <Hexagon q={0} r={-1} s={1} onClick={() => setLetters([...selectedLetters, letters[0]])}>
+            <Text>{letters[0]}</Text>
+          </Hexagon>
+          <Hexagon q={0} r={1} s={-1} onClick={() => setLetters([...selectedLetters, letters[1]])}>
+            <Text>{letters[1]}</Text>
+          </Hexagon>
+          <Hexagon q={1} r={-1} s={0} onClick={() => setLetters([...selectedLetters, letters[2]])}>
+            <Text>{letters[2]}</Text>
+          </Hexagon>
+
+          <Hexagon q={1} r={0} s={-1} onClick={() => setLetters([...selectedLetters, letters[3]])}>
+            <Text>{letters[3]}</Text>
+          </Hexagon>
+          <Hexagon q={-1} r={1} s={0} onClick={() => setLetters([...selectedLetters, letters[4]])}>
+            <Text>{letters[4]}</Text>
+          </Hexagon>
+          <Hexagon q={-1} r={0} s={1} onClick={() => setLetters([...selectedLetters, letters[5]])}>
+            <Text>{letters[5]}</Text>
+          </Hexagon>
+        </Layout>
+        <Pattern id="pat-1" link="http://cat-picture" />
+      </HexGrid>
+      <button onClick={() => setLetters([])}>Clear</button>
+      <button onClick={() => checkWord(selectedLetters.join(''))}>Submit</button>
+      <ul>{correctWords.map(word => (<li key={word}><h2>{word}</h2></li>))}</ul>
     </div>
   );
 }
---
+++ utils/app.js
@@ -0,0 +1,60 @@
+const files = [
+    './english-words-10.json',
+    './english-words-20.json',
+    './english-words-35.json',
+    './english-words-40.json',
+    './english-words-50.json',
+    './english-words-55.json',
+    './english-words-60.json',
+    './english-words-70.json'
+];
+
+let words = [];
+
+for (const file of files) {
+    words.push(...require(file));
+}
+words = words.sort();
+
+const pangrams = [];
+
+for (const word of words) {
+    const letters = new Set();
+    for (let i = 0; i < word.length; i++) {
+        letters.add(word.charAt(i));
+    }
+    const asciiLetters = [...letters].map(letter => letter.charCodeAt(0)).filter(charCode => charCode >= 97 && charCode <= 122);
+    if (asciiLetters.length === 7) {
+        pangrams.push({ word, letters: [...letters].sort() });
+    }
+}
+
+const setCounts = {};
+
+for(const pangram of pangrams) {
+    if (setCounts[pangram.letters]) {
+        setCounts[pangram.letters] += 1;
+    } else {
+        setCounts[pangram.letters] = 1;
+    }
+}
+
+// console.log(setCounts);
+
+// console.log(pangrams.length);
+
+const distinctPangrams = [];
+for(const pangram of pangrams) {
+    if (setCounts[pangram.letters] === 1) distinctPangrams.push(pangram);
+}
+
+// console.log(distinctPangrams.length);
+// console.log(JSON.stringify(distinctPangrams.map(pangram => pangram.word)));
+
+const asciiWords = [];
+for(const word of words) {
+    const asciiLetters = word.split('').map(letter => letter.charCodeAt(0)).filter(charCode => charCode >= 97 && charCode <= 122);
+    if (asciiLetters.length === word.length) asciiWords.push(word);
+}
+
+console.log(JSON.stringify(asciiWords));
